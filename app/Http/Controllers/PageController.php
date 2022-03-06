83ec1c3aa981306049a12d259fbc4cba514619c0
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-03-07 00:47:14 +0700

    PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        return "Selamat Datang !";
+    }
+
+    public function about(){
+        return "Moch Fajrul Falah 2041720070";
+    }
+
+    public function article($id){
+        return "Halaman Artikel dengan Id = ".$id;
+    }
 }
